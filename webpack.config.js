39f53e8593f33dd53abc6329a74afbed676c7512
pato <patriciosolana177@gmail.com>
2021-10-08 18:14:55 -0300

    prubea

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+    
+     entry: './frontend/app.js',
+     output: {
+         path: path.join(__dirname, 'backend/public'),
+         filename: 'bundle.js'
+     }
+
+};
